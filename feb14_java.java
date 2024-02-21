6ac1a2058dc83ee4ba3cec6ec0727265f59312a7
SurB1023 <surabhi10012002@gmail.com>
2024-02-21 10:38:20 +0530

    java files

---
+++ feb14_java.java
@@ -0,0 +1,88 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.io.*;
+import java.util.Scanner;
+class HelloWorld {
+   public static void main(String[] args) {
+        /** Scanner **/
+       /* System.out.print("Enter age:");
+        Scanner a= new Scanner(System.in);
+        int age= a.nextInt();
+        boolean isAdult=true;
+        if(age<18)
+        { 
+            isAdult=false;
+            System.out.print("Adult? :");
+            System.out.print(isAdult);
+        }
+        System.out.print("Adult? :");
+        System.out.print(isAdult);*/
+        
+        /** Dyanamic array **/
+         
+    /*    int[] ar= new int[5];
+        for(int i=0;i<5;i++)
+        {
+            Scanner sc = new Scanner(System.in);
+            ar[i]= sc.nextInt();
+        }
+       
+        for(int i=0;i<5;i++)
+           System.out.print(ar[i]); */
+           
+        /** taking string user inputs **/
+        /*
+        String name;
+        System.out.print("Name-");
+        Scanner sc = new Scanner(System.in);
+        name = sc.next();
+        System.out.print("Name-");
+        System.out.print(name); */
+        
+        /** multiply floating numbers **/
+        /*
+        float a;
+        float b;
+        System.out.print("Number 1: ");
+        Scanner sc1 = new Scanner(System.in);
+        a= sc1.nextFloat();
+        System.out.print("Number 2: ");
+        Scanner sc2 = new Scanner(System.in);
+        b= sc2.nextFloat();
+        
+        float ab;
+        ab= a*b;
+        System.out.print("Multiplicaion:");
+        System.out.print(ab); */
+        
+        /** Swap numbers **/
+       /*
+        int a;
+        int b;
+        
+        System.out.print("Number 1: ");
+        Scanner sc1 = new Scanner(System.in);
+        a= sc1.nextInt();
+        System.out.print("Number 2: ");
+        Scanner sc2 = new Scanner(System.in);
+        b= sc2.nextInt();
+        
+        int temp=0;
+        temp=a;
+        a=b;
+        b=temp;
+        
+        System.out.print("a :"+ a);
+       // System.out.print(a);
+        System.out.print("\n"+"b :"+ b);
+        //System.out.print(b);  */
+        
+  
+        
+        
+        
+        
+        
+    }
+  
+}
---
+++ feb15_java.java
@@ -0,0 +1,105 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.io.*;
+import java.util.Scanner;
+//class complex {
+/** add two binary strings **/
+    
+    /*static String fun(String x,String y)
+    {
+        int a =Integer.parseInt(x,2);
+        int b =Integer.parseInt(y,2);
+        int sum= a+b;
+
+        String result= Integer.toBinaryString(sum);
+        return result;
+        
+        
+    }
+      public static void main(String[] args)
+    {
+        String x="1010";
+        String y="0110";
+        String r=fun(x,y);
+        System.out.print(r);
+        
+    }
+    */
+    
+    /** complex number **/
+    /*int r,i;
+    public complex(int a,int b)
+    {
+        this.r=a;
+        this.i=b;
+    }
+    public void showc()
+    {
+        System.out.print(this.r+"+"+this.i+"i");
+    }
+    public static complex add(complex ob1,complex ob2)
+    {
+        complex res= new complex(0,0);
+        res.r= ob1.r+ob2.r;
+        res.i=ob1.i+ob2.i;
+        return res;
+        
+    }
+    
+    
+    public static void main(String[] args)
+    {
+         complex ob1,ob2;
+         ob1=new complex(5,10);
+         ob2= new complex(10,5);
+         complex res=add(ob1,ob2);
+         res.showc();
+        
+    }
+
+  
+}
+*/
+/** even or odd **/
+/*
+class hello{
+    
+    public static void main(String[] arg)
+    {
+        int number;
+        System.out.print("Enter number: ");
+        Scanner sc = new Scanner(System.in);
+        number = sc.nextInt();
+        
+        if(number %2==0)
+          System.out.print(number + " " + "is even.");
+        else
+        System.out.print(number + " "  + "is odd.");
+    }
+}
+*/
+/** largest among 3 **/
+/*class number
+{
+    
+    public static void main(String[] arg){
+        int n1,n2,n3;
+        System.out.print("Enter numbers: ");
+        Scanner sc1 = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+        Scanner sc3 = new Scanner(System.in);
+        n1 = sc1.nextInt();
+        n2 = sc2.nextInt();
+        n3 = sc3.nextInt();
+        
+        if(n1>n2 && n1>n3)
+         System.out.print(n1+ " is greater.");
+        else if(n1>n2 && n3>n1)
+         System.out.print(n3+ " is greater.");
+        else
+         System.out.print(n2+ " is greater.");
+        
+    }
+}
+
+  */
